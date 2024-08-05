54da550cfc87799e442684784df0350e3507df81
Victor Amajuoyi <victorgavon@gmail.com>
2024-08-06 00:50:43 +0100

    Basic API Authentication Task

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -7,10 +7,9 @@ from os import path
 import json
 import uuid
 
+
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
 DATA = {}
-
-T = TypeVar('T', bound='Base')
 
 
 class Base():
@@ -36,7 +35,7 @@ class Base():
         else:
             self.updated_at = datetime.utcnow()
 
-    def __eq__(self, other: T) -> bool:
+    def __eq__(self, other: TypeVar('Base')) -> bool:
         """ Equality
         """
         if type(self) != type(other):
@@ -110,20 +109,20 @@ class Base():
         return len(DATA[s_class].keys())
 
     @classmethod
-    def all(cls) -> Iterable[T]:
+    def all(cls) -> Iterable[TypeVar('Base')]:
         """ Return all objects
         """
         return cls.search()
 
     @classmethod
-    def get(cls, id: str) -> T:
+    def get(cls, id: str) -> TypeVar('Base'):
         """ Return one object by ID
         """
         s_class = cls.__name__
         return DATA[s_class].get(id)
 
     @classmethod
-    def search(cls, attributes: dict = {}) -> List[T]:
+    def search(cls, attributes: dict = {}) -> List[TypeVar('Base')]:
         """ Search all objects with matching attributes
         """
         s_class = cls.__name__
@@ -135,5 +134,5 @@ class Base():
                 if (getattr(obj, k) != v):
                     return False
             return True
-
+        
         return list(filter(_search, DATA[s_class].values()))
