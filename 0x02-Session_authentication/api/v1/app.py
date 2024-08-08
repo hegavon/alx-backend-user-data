511255a45fc04e919f5cfe0fed27a73de807e5b5
Victor Amajuoyi <victorgavon@gmail.com>
2024-08-09 00:40:49 +0100

    Session Authentication Task

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,15 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from flasgger import Swagger
-from models import storage
-from models.user import User
-from models.user_session import UserSession
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
-from api.v1.auth.session_db_auth import SessionDBAuth 
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
