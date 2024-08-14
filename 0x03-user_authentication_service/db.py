7db7921c400f1736d9da048f78dfe85bfbe9a518
Victor Amajuoyi <victorgavon@gmail.com>
2024-08-14 13:12:37 +0100

    User Authentication Service - Task 1. create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -28,17 +28,15 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database
-
-        Args:
-            email (str): The user's email address
-            hashed_password (str): The user's hashed password
-
-        Returns:
-            User: The created User object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        session = self._session
-        session.add(new_user)
-        session.commit()
-        return new_user
+        Create a User object and save it to the database
+        Args:
+            email (str): user's email address
+            hashed_password (str): password hashed by bcrypt's hashpw
+        Return:
+            Newly created User object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
